7ee4f24f208d56d8720bd63a81cc69896c796ecc
Michael Weber <webermn15@gmail.com>
2017-12-08 14:21:05 -0600

    inital commit, problem 1 solved

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// PROJECT ONE
+
+let multiplesArray = [];
+let total = 0;
+
+for (let i = 0; i <= 1000; i++) {
+	if (i % 3 === 0 || i % 5 === 0) {
+		multiplesArray.push(i);
+	}
+}
+
+console.log(multiplesArray.length);
+
+for (let i = 0; i < multiplesArray.length; i++) {
+	total += i;
+}
+
+console.log(total);
